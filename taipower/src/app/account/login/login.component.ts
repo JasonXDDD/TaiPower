c010eb740f0f902ab0f18d1c5a9617ce7f04e342
Len_AshBell <jason1022liu@gmail.com>
2020-01-13 20:08:01 +0800

    [device] update device login routing

--- taipower/src/app/account/login/login.component.ts
+++ taipower/src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private deviceService: DeviceDetectorService) { }
 
   ngOnInit() {
+    let self = this
+    // do route if mobile
+    this.checkRoute(this.router.url)
+    this.router.events.subscribe(async evt => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      else {
+        self.checkRoute(evt.url)
+      }
+    })
+  }
+
+  checkRoute(url){
+    if(this.deviceService.isMobile() && url.split('/')[1] !== 'mobile'){
+      this.router.navigate(['/mobile/index'])
+    }
   }
 
 }
